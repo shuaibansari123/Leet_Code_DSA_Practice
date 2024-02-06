b6ebc0672c3618faa351ae9accc905664bc506a3
shuaib-onorall <shuaib.onorall2k2@gmail.com>
2024-02-06 12:49:56 +0530

    s

--- anagramGroup_0001_medium.py
+++ anagramGroup_0001_medium.py
@@ -49,7 +49,6 @@ class Solution:
 class Solution:
   def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
     dict = collections.defaultdict(list)
-
     for str in strs:
       key = ''.join(sorted(str))
       dict[key].append(str)
