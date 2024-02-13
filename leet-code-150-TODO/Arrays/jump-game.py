75a3a7218b3b7dfc283f1b53538b1f748ad21c93
shuaib-onorall <shuaib.onorall2k2@gmail.com>
2024-02-13 13:40:15 +0530

    ARRAY:- h-index,jump,

--- leet-code-150-TODO/Arrays/jump-game.py
+++ leet-code-150-TODO/Arrays/jump-game.py
@@ -8,7 +8,7 @@ Each element nums[i] represents the maximum length of a forward jump from index 
 i + j < n
 Return the minimum number of jumps to reach nums[n - 1]. The test cases are generated such that you can reach nums[n - 1].
 
- 
+
 
 Example 1:
 
